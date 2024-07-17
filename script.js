9e8348d3507ff007915482553fd840a041b96086
NicollasProvatti <74670755+nicollas18@users.noreply.github.com>
2024-07-17 18:40:08 -0300

    Zoom images

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-function Zoom() {
+function Zoom(i) {
     const modal = document.getElementById('modal');
+    const img = document.getElementById('modalImg');
+
+    if (i == undefined) {
+        modal.className = modal.className.replace(" modalDisplay", "");
+        return;
+    }
+    
+    img.src = `images/foto${i}.jpeg`;
 
     if (modal.className.indexOf("modalDisplay") == - 1) {
         modal.className += " modalDisplay";
-        console.log("if")
+        
     } else {
         modal.className = modal.className.replace(" modalDisplay", "");
-        console.log("else")
+
     }
 }
